bd1df7c472d742f9cadfc19bd16bac88b10d6bb0
MohdTamer <153457780+MohdTamer@users.noreply.github.com>
2025-08-10 17:09:34 +0300

    Add files via upload

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Category => Set<Category>();
+        public DbSet<Cart> Cart => Set<Cart>();
+        public DbSet<CartItem> CartItem => Set<CartItem>();
+    }
+}
